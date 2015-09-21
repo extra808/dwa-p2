be4b3d9b39211fdb1fa72cb2a1d3cbc98bda3288
Curtis Wilcox <extra808@m.cognize.org>
2015-09-20 22:55:36 -0400

    add qtyWords blur event, remove jQuery dependency

--- htdocs/javascripts/select-all-on-click.js
+++ htdocs/javascripts/select-all-on-click.js
@@ -3,16 +3,41 @@
  *
  * Couldn't get script to select all text on focus event.
  */
-
+var pass = document.getElementById('passphrase');
+var qtyW = document.getElementById('qtyWords');
+var qtyS = document.getElementById('qtySymbols');
+var qtyD = document.getElementById('qtyDigits');
 
 var clickListener = function( event ) {
                     // select all text in element
                     event.target.setSelectionRange(0, this.value.length);
                     // stop listening, allows selection of a portion of text
-                    event.target.removeEventListener('click', clickListener); }
+                    event.target.removeEventListener('click', clickListener);
+                    }
 
-passphrase.addEventListener('click', clickListener );
+pass.addEventListener('click', clickListener );
 
-passphrase.addEventListener('blur', function( event ) { 
+pass.addEventListener('blur', function( event ) {
                     // start listening again
-                    event.target.addEventListener('click', clickListener ); } );
+                    event.target.addEventListener('click', clickListener );
+                    } );
+
+/*!
+ * Limit # of symbols and digits to no greater than # of words
+ *
+ */
+qtyW.addEventListener('blur', function( event ) {
+                    var newMax;
+                    if (event.target.valueAsNumber <= 1) {
+                        newMax = 1;
+                        }
+                    if (event.target.valueAsNumber >= 9) {
+                        newMax = 9;
+                        }
+                    else {
+                        newMax = event.target.value;
+                        }
+
+                    qtyS.setAttribute("max", newMax);
+                    qtyD.setAttribute("max", newMax);
+                    } );
